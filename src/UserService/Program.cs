ecdd08bc71e640ab5a759ed06f6d77cba9d55162
platon-reshetnikov <82944686+platon-reshetnikov@users.noreply.github.com>
2025-10-08 01:52:40 +0300

    fix2

--- src/UserService/Program.cs
+++ src/UserService/Program.cs
@@ -1,8 +1,5 @@
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using UserService.Data;
